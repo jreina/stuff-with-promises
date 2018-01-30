c10afeb2ce10b9b72d634a32099e4fb11c80d592
Johnny Reina <johnny.r.reina@gmail.com>
2018-01-29 22:52:21 -0600

    Names Promise chain functions for #1

--- usingPromises.js
+++ usingPromises.js
@@ -1,22 +1,39 @@
-const api = require('./api-promise');
-const $ = require('./ui');
+const api = require("./api-promise");
+const $ = require("./ui");
+
+const templates = { nameInAList: peep => $(`<li>${peep.name}</li>`) };
 
 const peopleList = $('<ul class="no-fur-owners"></ul>');
 
-const getPeople = types =>
+const getPeopleForAnimalTypes = types =>
   api.getPerson(person =>
     person.pets.some(pet => {
       return types.includes(pet.type);
     })
   );
 
+const extractAnimalTypes = furlessAnimals =>
+  furlessAnimals.map(animal => animal.type);
+
+const createUiElements = template => peopleWithFurlessPets => {
+  return peopleWithFurlessPets.map(template);
+};
+
+const tap = f => x => {
+  f(x);
+  return x;
+};
+
+const appendElements = list => listItems => listItems.forEach(list.append);
+
+const writeCompiledHtml = () => console.log(peopleList.compile());
+
+const withoutFur = animal => !animal.fur;
+
 api
-  .getAnimal(animal => !animal.fur)
-  .then(furlessAnimals => furlessAnimals.map(animal => animal.type))
-  .then(animalTypes => getPeople(animalTypes))
-  .then(peeps => Promise.all(peeps))
-  .then(peopleWithFurlessPets => {
-    return peopleWithFurlessPets.map(peep => $(`<li>${peep.name}</li>`));
-  })
-  .then(listItems => listItems.forEach(peopleList.append))
-  .then(() => console.log(peopleList.compile()));
+  .getAnimal(withoutFur)
+  .then(extractAnimalTypes)
+  .then(getPeopleForAnimalTypes)
+  .then(createUiElements(templates.nameInAList))
+  .then(tap(appendElements(peopleList)))
+  .then(tap(writeCompiledHtml));
